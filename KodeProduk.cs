9ad1ffbdb6e005bf04b55be784396b41fc16abd2
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:02:53 +0700

    menambahkan class kodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300046
+{
+    class KodeProduk
+    {
+        public enum Elektronik
+        {
+            Laptop, Smartphone, Tablet, Headset, Keyboard, Mouse, Printer, Monitor, Smartwatch, Kamera
+        }
+
+        public static string getKodeProduk(Elektronik elektronik)
+        { 
+            string[] kodeProduk = {"E100", "E101",  "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109"};
+            return kodeProduk[(int)elektronik];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul4_103022300046
+{
+    static void Main(string[] args)
+    {
+        //soal 1 table driven
+        Console.Write("Masukkan nama produk elektronik: ");
+        string inputElektronik = Console.ReadLine();
+
+        KodeProduk.Elektronik elektronik;
+        bool isValidElektronik = Enum.TryParse(inputElektronik, true, out elektronik);
+
+        if (isValidElektronik)
+        {
+            string kodeProduk = KodeProduk.getKodeProduk(elektronik);
+            Console.WriteLine($"Kode produk untuk {inputElektronik} : {kodeProduk}")s;
+        }
+        else
+        {
+            Console.WriteLine("produsk elektronik tidak ada");
+        }
+
+
+    }
+}
