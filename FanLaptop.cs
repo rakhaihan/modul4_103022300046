85e9a06552b6d657c572f281adf05f96d01a4eea
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 08:01:03 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300046
+{
+    class FanLaptop
+    {
+        public enum State {Quiet, Balanced, Performance, Turbo };
+        public enum Trigger {Mode_down, Mode_up, Turbo_shortcut };
+
+        struct transition
+        {
+            public State prevState;
+            public State nextState;
+            public Trigger trigger;
+
+            public transition(State prevState, State nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+
+
+            transition[] transitions =
+            {
+                new transition(State.Quiet, State.Balanced, Trigger.Mode_up),
+                new transition(State.Quiet, State.Turbo, Trigger.Turbo_shortcut),
+                new transition(State.Balanced, State.Performance, Trigger.Mode_up),
+                new transition(State.Balanced, State.Quiet, Trigger.Mode_down),
+                new transition(State.Performance, State.Balanced, Trigger.Mode_down),
+                new transition(State.Performance, State.Turbo, Trigger.Mode_up),
+                new transition(State.Turbo, State.Quiet, Trigger.Turbo_shortcut),
+                new transition(State.Turbo, State.Performance, Trigger.Mode_down)
+            };
+
+            public State currentState = State.Quiet;
+
+            public State getNextState(State prevState, Trigger trigger)
+            {
+                State nextState = prevState;
+
+                for (int i = 0; i < transitions.Length; i++)
+                {
+                    if (transitions[i].prevState == prevState && transitions[i].trigger == trigger)
+                        nextState = transitions[i].nextState;
+                }
+
+                return nextState;
+            }
+
+            public void activate(Trigger trigger)
+            {
+                State nextState = getNextState(currentState, trigger);
+                currentState = nextState;
+            }
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,25 +2,107 @@
 
 namespace modul4_103022300046
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        //soal 1 table driven
-        Console.Write("Masukkan nama produk elektronik: ");
-        string inputElektronik = Console.ReadLine();
-
-        KodeProduk.Elektronik elektronik;
-        bool isValidElektronik = Enum.TryParse(inputElektronik, true, out elektronik);
-
-        if (isValidElektronik)
+        static void Main(string[] args)
         {
-            string kodeProduk = KodeProduk.getKodeProduk(elektronik);
-            Console.WriteLine($"Kode produk untuk {inputElektronik} : {kodeProduk}")s;
-        }
-        else
-        {
-            Console.WriteLine("produsk elektronik tidak ada");
-        }
+            //soal 1 table driven
+            Console.Write("Masukkan nama produk elektronik: ");
+            string inputElektronik = Console.ReadLine();
 
+            KodeProduk.Elektronik elektronik;
+            bool isValidElektronik = Enum.TryParse(inputElektronik, true, out elektronik);
+
+            if (isValidElektronik)
+            {
+                string kodeProduk = KodeProduk.getKodeProduk(elektronik);
+                Console.WriteLine($"Kode produk untuk {inputElektronik} : {kodeProduk}");
+            }
+            else
+            {
+                Console.WriteLine("produk elektronik tidak ada");
+            }
+
+
+
+            //soal 2 state based
+            FanLaptop fanLaptop = new FanLaptop();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine($"State saat ini: {fanLaptop.currentState}");
+                Console.WriteLine("masukkan trigger: Mode_down, Mode_up, Turbo_shortcut, exit");
+                string inputFan = Console.ReadLine();
+
+                switch (inputFan)
+                {
+                    case "Mode_up":
+                        if (fanLaptop.currentState == FanLaptop.State.Quiet)
+                        {
+                            Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+                            fanLaptop.activate(FanLaptop.Trigger.Mode_up);
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Balanced)
+                        {
+                            Console.WriteLine("Fan Balanced berubah menjadi Performance");
+                            fanLaptop.activate(FanLaptop.Trigger.Mode_up);
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Performance)
+                        {
+                            Console.WriteLine("Fan Performance berubah menjadi Turbo");
+                            fanLaptop.activate(FanLaptop.Trigger.Mode_up);
+                        }
+                        //fanLaptop.activate(FanLaptop.Trigger.Mode_up);
+                        break;
+
+                    case "Mode_down":
+                        if (fanLaptop.currentState == FanLaptop.State.Balanced)
+                        {
+                            Console.WriteLine("Fan Balanced berubah menjadi Quiet");
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Performance)
+                        {
+                            Console.WriteLine("Fan Performance berubah menjadi Balanced");
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Turbo)
+                        {
+                            Console.WriteLine("Fan Turbo berubah menjadi Performance");
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Quiet)
+                        {
+                            Console.WriteLine("Fan Quiet berubah menjadi Balance");
+                        }
+                        fanLaptop.activate(FanLaptop.Trigger.Mode_down);
+
+                        break;
+
+                    case "Turbo_shortcut":
+                        if (fanLaptop.currentState == FanLaptop.State.Quiet)
+                        {
+                            Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+                        }
+                        else if (fanLaptop.currentState == FanLaptop.State.Turbo)
+                        {
+                            Console.WriteLine("Fan Turbo berubah menjadi Quiet");
+                        }
+
+                        fanLaptop.activate(FanLaptop.Trigger.Turbo_shortcut);
+                        break;
+
+                    case "exit":
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("perintah tidak valid");
+                        break;
+                }
+
+            }
+
+        }
 
     }
 }
